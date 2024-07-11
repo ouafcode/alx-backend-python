ca781faa020b79bc9e21c22b83a7aa437752aac2
ouafcode <ouafaesaim@gmail.com>
2024-07-11 21:06:50 +0000

    task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" concat two string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat two string and returns the result.
+
+    Args:
+       str1 (string) : the first string.
+       str2 (string) : the second string.
+
+    Returns:
+       string : the concat of str1 and str2.
+    """
+    return str1 + str2
