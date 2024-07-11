f972c2363398050cf15463d62a6007808b827fa1
ouafcode <ouafaesaim@gmail.com>
2024-07-11 21:16:23 +0000

    task2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Basic annotations - floor """
+
+
+def floor(n: float) -> int:
+    """
+    Basic annotations - floor
+
+    Args:
+       n (float) : the first number.
+
+    Returns:
+       int : float of float.
+    """
+    return int(n)
