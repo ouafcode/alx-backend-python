99a4291d58a05465e840b7c9a70e432c378742c7
ouafcode <ouafaesaim@gmail.com>
2024-07-12 11:29:16 +0000

    task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string """
+
+
+def to_str(n: float) -> str:
+    """
+    Basic annotations - to string
+
+    Args:
+       n (float) : the first number.
+
+    Returns:
+       str : string representation of float.
+    """
+    return str(n)
