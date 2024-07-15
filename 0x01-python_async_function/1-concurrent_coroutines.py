fdc0afc5489ffa476c6e2fc1eb1d6c4e1dcc309a
ouafcode <ouafaesaim@gmail.com>
2024-07-15 13:41:59 +0000

    task1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" The basics of async """
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    lists = []
+    for _ in range(n):
+        delay = await(wait_random(max_delay))
+        lists.append(delay)
+    return sorted(lists)
